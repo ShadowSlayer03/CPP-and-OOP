e4b36c02b61da87fc0ba12ae2ba62c9ce6a2b3c6
Arjun R Nambiar <95574870+ShadowSlayer03@users.noreply.github.com>
2023-05-25 00:51:19 +0530

    namespace std

---
+++ Basic/use_of_namespace_std.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+  std::cout << "Hello World!\n";
+  cout << "Using std:: always is not very awesome.\nSo we use 'using namespace std' before main function";
+}
