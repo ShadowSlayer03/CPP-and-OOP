23a9b4281e45d5bd41d14e34c9ecb3c2d6d4613b
Arjun R Nambiar <95574870+ShadowSlayer03@users.noreply.github.com>
2023-05-24 23:24:01 +0530

    cout in CPP

---
+++ Basic/cout.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int a= 89;
+  cout << "This is how we print a string literal.\n";
+  cout << "This is how we print an integer directly "<<23 <<"\n";
+  cout <<"This is how we print a variable "<<a <<"\n";
+  return 0;
+}
