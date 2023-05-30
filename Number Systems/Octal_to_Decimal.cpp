f0e9e2b117523abe3e4d111828b1acf685d6a705
Arjun R Nambiar <95574870+ShadowSlayer03@users.noreply.github.com>
2023-05-30 23:07:00 +0530

    Convert Octal to Decimal

---
+++ Number Systems/Octal_to_Decimal.cpp
@@ -0,0 +1,20 @@
+// Octal to Decimal
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+  int i=0,num,rem,sum=0;
+  cout<<"Enter the octal no"<<endl;
+  cin>>num;
+  while(num!=0)
+    {
+      rem=num%10;
+      sum=sum+(rem*pow(8,i));
+      i++;
+      num=num/10;
+    }
+  cout<<"Required Decimal no is "<<sum<<endl;
+  return 0;
+}
