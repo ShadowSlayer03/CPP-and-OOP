16ede1e8b8531e1fb310aeec31a69efccc975cd1
Arjun R Nambiar <95574870+ShadowSlayer03@users.noreply.github.com>
2023-06-18 23:34:42 +0530

    Protected Way of Inheritance

---
+++ Inheritance/Ways_of_Inheritance3-Protected.cpp
@@ -0,0 +1,46 @@
+//Ways of Inheritance - Protected
+//Public of parent = Protected of child
+//Protected of parent = Protected of child
+//Private of parent = Inaccessible by child
+#include<iostream>
+using namespace std;
+
+class Parent
+{
+private:
+  int x;
+public:
+  int y;
+protected:
+  int z;
+};
+
+class Child:protected Parent
+{
+public:
+  void set()
+{
+  //x=9;  Private so cannot be accessed
+  y=10;
+  z=11;
+}
+};
+
+class grandChild:public Child
+{
+public:
+  void change()
+{
+  //x=90; Private so cannot be accessed
+  y=100;  
+  z=110; 
+  //These values are protected wrto class 'Child' so they can be modified
+}
+};
+
+int main()
+{
+  grandChild g1;
+  g1.change();
+  return 0;
+}
