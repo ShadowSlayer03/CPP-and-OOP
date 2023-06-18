9d4de3455fe9af4753baf1d5d5994d2126d46eeb
Arjun R Nambiar <95574870+ShadowSlayer03@users.noreply.github.com>
2023-06-18 19:20:18 +0530

    Added some extra methods

--- STL/vectors_in_detail.cpp
+++ STL/vectors_in_detail.cpp
@@ -16,23 +16,40 @@ int main()
     cout<<x<<" ";
 
 // ACCESS INDIVIDUAL VALUES:
-  cout<<endl;
   cout<<vc[0]<<endl;  //  or
   cout<<vc.at(0)<<endl;
 
-// ADD NEW VALUES:
+// ADD NEW VALUES AT THE END OF VECTOR:
   int a=8;
   v.push_back(12.7);
   v.push_back(13.9);
   vc.push_back(a);
 
+//ADD VALUES AT ANY POSITION IN THE VECTOR:
+// We can do this by using insert or emplace methods. The diff b/w them is the way in which they work.
+// Insert creates a copy of that element and places it in the desired location while emplaces just takes that element and places directly. 
+// Both shift the remaining elements to the right before inserting at a desired location.
+  v.insert(v.begin()+2,54);
+  v.emplace(v.begin()+3,67);
+
+//TAKE OUT VALUES FROM THE END OF VECTOR:
+  v.pop_back();  
+
+// ERASE A SINGLE VALUE FROM A DESIRED POSITION OR AN ENTIRE SET OF VALUES FROM THE VECTOR:
+  v.erase(v.begin()+7);
+  v.erase(v.begin()+1,v.begin()+3);
+
 //FIRST AND LAST ELEMENT:
-cout<<v.front()<" "<<v.back()<<endl;  
+cout<<v.front()<<" "<<v.back()<<endl;  
   
 // CHECK SIZE:
   cout<<"Size of v = "<<v.size()<<endl;
   cout<<"Size of vc = "<<vc.size()<<endl;
   cout<<"Size of vctr = "<<vctr.size()<<endl;
+
+//CLEAR THE VECTOR FROM MEMORY
+  v.clear();
+  cout<<v.size()<<endl;  
 
   return 0;
 }
