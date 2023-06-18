d1f57b4825eae74b32ae57eedba8c77970946903
Arjun R Nambiar <95574870+ShadowSlayer03@users.noreply.github.com>
2023-06-19 01:27:40 +0530

    Find ASCII value of a character

---
+++ Lab Programs/ascii.cpp
@@ -0,0 +1,12 @@
+// ASCII VALUE
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  char c;
+  cout<<"Enter a character"<<endl;
+  cin>>c;
+  cout<<"ASCII value of character is "<<(int)c<<endl;
+  return 0;
+}
