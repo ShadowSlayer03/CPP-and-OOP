ea4b9b14abed5c79cb9ebe82886a04be2cd68746
Arjun R Nambiar <95574870+ShadowSlayer03@users.noreply.github.com>
2023-06-23 20:43:03 +0530

    Create try_catch_question.cpp

---
+++ Lab Programs/try_catch_question.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+void division(float a,float b)
+{
+  try
+  {
+    if(b==0)
+      throw 1;
+    else
+      cout<<"The quotient of the 2 numbers is "<<a/b<<endl;
+  }
+  catch(int)
+    {
+      cout<<"Division by zero undefined"<<endl;
+    }
+}
+
+void readNums()
+{
+  float a,b;
+  cout<<"Enter the 2 floating point numbers:"<<endl;
+  try{
+    if(cin>>a>>b)
+    {
+      division(a,b);
+    }
+    else
+      throw 5;
+  }
+  catch(int)
+    { 
+      cout<<"Invalid input: Not a float"<<endl;
+    }
+}
+
+int main()
+{
+  readNums();
+  return 0;
+}
+
+
+
